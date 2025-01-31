3b8c7d774c6c88228cc5f29d921d5ef62c0ff1c6
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-01-31 11:39:57 +0100

    Day 2

---
+++ 2025/index.js
@@ -0,0 +1,20 @@
+// loops
+
+// 1. while loopinde
+let i = 0;
+while(i < 10) {
+    console.log("Number: ", i);
+    i = i + 1;
+}
+
+// 2. For loop
+
+for (let i = 0; i < 10; i++) {
+    console.log("Number: ", i);
+}
+
+// Infinity love loop---------This is just me fooling around
+
+// for (let love = 1; love >= 1; love++) {
+// console.log("My Love is infinit times: ", love)
+// }
