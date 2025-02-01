59daa8e8046ca1fbea8f50dae6a465f8651ad5d7
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-02-01 03:19:31 +0100

    1/02/25

--- 2025/index.js
+++ 2025/index.js
@@ -1,33 +1,37 @@
-// loops
+//------- loops
 
-// 1. while loopinde
+//---------- 1. while loop
+
 // let i = 0;
 // while(i < 10) {
 //     console.log("Number: ", i);
 //     i = i + 1;
 // }
 
-// 2. For loop
+//------------ 2. For loop
 
 // for (let i = 0; i < 10; i++) {
 //     console.log("Number: ", i);
 // }
 
-// Infinity love loop---------This is just me fooling around
+//----------- Infinity love loop---------This is just me fooling around
 
 // for (let love = 1; love <= 1; love++) {
 // console.log("My Love is infinite times: ", love)
 // }
 
-// Functions
+//-------- Functions
 
-function printNumbers(firstNum, lastNum) {
-    let i = firstNum;
+// function printNumbers(firstNum, lastNum) {
+//     let i = firstNum;
 
-    while(i <= lastNum) {
-    console.log("Number: ", i);
-    i = i + 1;
-}
-}
+//     while(i <= lastNum) {
+//     console.log("Number: ", i);
+//     i = i + 1;
+//     }
+// }
 
-printNumbers(50, 100);
+// printNumbers(50, 100);
+
+
+//---------------- D O M
