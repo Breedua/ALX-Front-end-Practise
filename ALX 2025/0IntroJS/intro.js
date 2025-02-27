a7706d0faee94390e1c09ebb235c3f5585bab476
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-02-27 23:01:25 +0100

    Added a readme file

---
+++ ALX 2025/0IntroJS/intro.js
@@ -0,0 +1,53 @@
+console.log("Hello World!!");
+
+//variable keyword
+// var, let, const
+
+//var
+var name = 'Eduaina'
+
+name = 'Ighalo'
+
+console.log(name); 
+
+//let
+let names = 'Eduaina'
+
+console.log(names);
+
+//const
+const numero = 200
+
+console.log(numero);
+
+//functions
+function greeting(names) {
+    names = 'Eduaina Brenda Ighalo'
+
+    return `Hello ${names}`;
+}
+
+console.log (greeting());
+
+// DATA TYPES
+
+// 1. String 
+'An example of a string'
+"Another example of a string"
+
+// 2. Numbers
+29.118
+30
+
+// 3. Boolean
+true/false
+
+// 4. Array
+['0, january, 5, februrary']
+
+// 5. Objects
+const person = {
+    fistName: 'Bee',
+    age: 22,
+    houseaddress: Benincity
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center">Edua the ALX Frontend Dev 2025</h1>
+<br>
+<br>
+<h3>Self practice for all AlX weeks from Week 1-17</h3>
+
+Fully practising:
+1. git(Version Control Systems)
+2. Collaborations with git and github
+3. Designs
+4. Html Basics and Advanced
+5. CSS Basics and Advanced
+6. Javascript and React
