ba1faa6da23d790b0986c8ea638b5d6f859628fb
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-03-09 17:08:08 +0100

    Week 6, Excercise 1 completed succesfully

---
+++ Practice lessons/Week 6/Excercise 1/ex1.js
@@ -0,0 +1,31 @@
+//string
+let name = 'Eduaina Brenda Ighalo';
+console.log(name, typeof name);
+
+//Number
+let age = 29;
+console.log(age, typeof age);
+
+//Boolean
+let isStudent = false;
+console.log(isStudent, typeof isStudent);
+
+//Object
+let address = {
+    city: ['Ekpoma', 'Edo State'],
+    country: 'Nigeria'
+};
+console.log(address, typeof address);
+
+//Array
+let hobbies = ['Reading', 'Sleeping', 'Gisting', 'Watching Kdrama', 'Watching Anime', 'boardGames','Video Games'];
+console.log(hobbies, typeof hobbies);
+
+//Undefined
+let undefinedVariable;
+console.log(undefinedVariable, typeof undefinedVariable);
+
+//Null
+let nullVariable = null;
+console.log(nullVariable)
+
